94fe15fc7bec13496298fe2d193e994ade6d3555
KazeVentum <132288586+KazeVentum@users.noreply.github.com.>
2023-09-21 22:13:21 -0500

    feat: :sparkles: Creacion de constantes e Inicializacion de API
    @KazeVentum

---
+++ main.js
@@ -0,0 +1,9 @@
+// Creacion de Constantes para DOM al interactuar
+const formulario = document.querySelector("#formAdd");
+const formEdit = document.querySelector("#formEdit");
+const dialog = document.querySelector("dialog");
+
+console.log(dialog)
+
+// URL de API MockAPI Requests and Response
+const url = "https://650b10a4dfd73d1fab098284.mockapi.io/tab"
